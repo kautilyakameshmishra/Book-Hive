5cde439a4d8836687db7ad26997ca121f2097626
Kautilya Mishra <kautilyakamesh3@gmail.com>
2024-03-15 13:51:22 +0530

    Completed Server Side Code

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL=
+'mongodb+srv://kautilyakamesh3:kautilya@book-store-project.avxjyrx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Project'
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+import Cors from 'cors';
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling CORS POLICY
+// option 1
+//app.use(Cors());
+// option 2 custom option
+app.use(
+    Cors({
+        origin: 'http://localhost:5555',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Tyoe'],
+}));
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('welcome');
+});
+
+app.use('/books', booksRoute);
+
+mongoose.connect(mongoDBURL)
+.then(()  => {
+    console.log('App is connected to database');
+    app.listen(PORT, () => {
+        console.log('App is listening to port: ${PORT}');
+    });
+})
+.catch((error)  => {
+    console.log(error);
+});
+
