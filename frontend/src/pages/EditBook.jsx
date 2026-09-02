c3fd1dbc7ee314fd085eceffbaedbdaa784858ef
Kautilya Mishra <91334392+kautilyakameshmishra@users.noreply.github.com>
2026-09-02 12:53:37 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      axios.put(`https://book-hive-nzp6.onrender.com/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
